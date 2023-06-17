b02d30252dd2e6edba890108cf2e153c3b2ee263
Yuri Diego <accprofissional.ya@gmail.com>
2023-06-17 02:37:25 -0300

    Adicionado o Metodo no listarTudo.py

--- listarTudo.py
+++ listarTudo.py
@@ -2,3 +2,16 @@ from operacoesbd import *
 
 def listarTodasAsManifestacoes(conexao):
     print()
+    comandoSql = "select count(*) from manifestacao"
+    manifestacoes = listarBancoDados(conexao, comandoSql)
+    manifestacoes = manifestacoes[0][0]
+
+    if manifestacoes == 0:
+        print('Não há manifestações a serem listadas!')
+
+    else:
+        comandoSql = "select * from manifestacao"
+        manifestacoes = listarBancoDados(conexao, comandoSql)
+
+        for i in manifestacoes:
+            print('Código', i[0], '-', i[1], '-', i[4], '-', i[3])
