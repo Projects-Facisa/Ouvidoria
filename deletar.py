1bab6c3d8988fde753d09005a3da5b4d9ffb82a0
svneto <severino.neto465@gmail.com>
2023-06-18 17:05:22 -0300

    corrigido algumas inconsistências no feedback

--- deletar.py
+++ deletar.py
@@ -5,25 +5,27 @@ def deletarManifestacao(conexao):
     while codigo != 0:
         try:
             print()
-            codigo = int(input('digite o codigo da manifestação ou 0 para voltar: '))
+            codigo = int(input('Digite o codigo da manifestação ou 0 para voltar: '))
             consultaListagem = 'select * from manifestacao where codigo = ' + str(codigo)
             listarPorCodigo = listarBancoDados(conexao, consultaListagem)
             if codigo == 0:
                 print()
-                print('voltando...')
+                print('Voltando...')
             elif len(listarPorCodigo) == 0:
                 print()
-                print('não há manifestações neste codigo')
+                print('Não há manifestações neste codigo!')
             else:
-                print('deseja exlucir a manifestação abaixo?')
+                print()
+                print('Deseja exlucir a manifestação abaixo?')
                 for i in listarPorCodigo:
+                    print()
                     print('Código:', i[0])
                     print('Título:', i[1])
                     print('Descrição:', i[2])
                     print('Tipo:', i[3])
                     print('Autor:', i[4])
                     print()
-                    print('1)sim 2)não')
+                    print('1)Sim 2)Não')
 
                     opcao = 'entrada'
                     while opcao != 2:
@@ -33,12 +35,12 @@ def deletarManifestacao(conexao):
                             valores = [codigo]
                             excluirBancoDados(conexao, deletarSql, valores)
                             print()
-                            print('manifestação removida!')
+                            print('Manifestação removida com sucesso!')
                             opcao = 2
                         elif opcao == 2:
                             print()
-                            print('voltando...')
+                            print('Voltando...')
 
         except ValueError:
             print()
-            print('código ou opção inválido(a)!')
+            print('Código ou opção inválido(a)!')
