489d7b353b3415b5796300f3512aa85f5d9579ca
svneto <severino.neto465@gmail.com>
2023-06-26 00:53:13 -0300

    Corrigido uma condição para voltar ao menu principal, adcionado comentários gerais sobre algumas linhas e feedback novo na confirmação de deleção.

--- deletar.py
+++ deletar.py
@@ -1,17 +1,22 @@
 from operacoesbd import *
+'''
+linha 14 pede int para as entradas no if ficaram como int, e para rodar no sql sem problema caso
+a pessoa bote uma letra para o sql pesquisar na column codigo,
+depois transforma em string na linha 15 para integrar no codigo do sql.
+'''
+
 
 def deletarManifestacao(conexao):
-    codigo = 'entrada'
+    codigo = 'entrada'  # variavel para rodar o while abaixo
     while codigo != 0:
         try:
-            print()
             codigo = int(input('Digite o codigo da manifestação ou 0 para voltar: '))
-            consultaListagem = 'select * from manifestacao where codigo = ' + str(codigo)
-            listarPorCodigo = listarBancoDados(conexao, consultaListagem)
-            if codigo == 0:
+            consultaListagem = 'select * from manifestacao where codigo = ' + str(codigo)  # código sql para pegar a row
+            listarPorCodigo = listarBancoDados(conexao, consultaListagem)  # criada a variavel que pega o valor da row
+            if codigo == 0:  # feedaback para retorno
                 print()
                 print('Voltando...')
-            elif len(listarPorCodigo) == 0:
+            elif len(listarPorCodigo) == 0:  # verifica o tamanho da row para dar feedaback caso esteja vazia
                 print()
                 print('Não há manifestações neste codigo!')
             else:
@@ -27,20 +32,26 @@ def deletarManifestacao(conexao):
                     print()
                     print('1)Sim 2)Não')
 
-                    opcao = 'entrada'
-                    while opcao != 2:
-                        opcao = int(input(': '))
-                        if opcao == 1:
-                            deletarSql = 'delete from manifestacao where codigo = %s'
-                            valores = [codigo]
-                            excluirBancoDados(conexao, deletarSql, valores)
-                            print()
-                            print('Manifestação removida com sucesso!')
-                            opcao = 2
-                        elif opcao == 2:
-                            print()
-                            print('Voltando...')
+                    opcao = 'entrada'  # variavel para rodar o while abaixo
+                    while opcao != 2:  # while para continuar pedindo o input caso digite código inválido ou string
+                        try:
+                            opcao = int(input(': '))
+                            if opcao == 1:
+                                deletarSql = 'delete from manifestacao where codigo = %s'  # código sql para pegar a row específica do código inserido
+                                valores = [codigo]
+                                excluirBancoDados(conexao, deletarSql, valores)  # executa a linha 41 no servidor sql
+                                print()
+                                print('Manifestação removida com sucesso!')
+                                opcao = 2
+                            elif opcao == 2:
+                                print()
+                                print('Voltando...')
+                            elif opcao != 2:
+                                raise ValueError  # caso a opcao não for 1 ou 2 vai pro mesmo except como se fosse string
+                        except ValueError:
+                            print('Opção Invalida! Tente : (1).Sim (2).Não ')
+                        codigo = 0
 
         except ValueError:
             print()
-            print('Código ou opção inválido(a)!')
+            print('Código inválido!')
