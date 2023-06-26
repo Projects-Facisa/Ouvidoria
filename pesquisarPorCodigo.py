7fa916d60377f171a44e33ca7db45e9c71ed4ca1
svneto <severino.neto465@gmail.com>
2023-06-26 14:50:22 -0300

    adcionado estrutura de repetição para retornar ao menu caso deseje, padronizando com outras opções que pedem input do codigo

--- pesquisarPorCodigo.py
+++ pesquisarPorCodigo.py
@@ -1,15 +1,17 @@
 from operacoesbd import *
 
 def pesquisarManifestacaoPorCodigo(conexao):
-
-        codigo = int(input('Digite o código da manifestação que desejada listar: '))
-        print()
-
+    codigo = 'entrada'
+    while codigo != 0:
         try:
+            codigo = int(input('Digite o codigo da manifestação ou 0 para voltar: '))
             comandoSql = "select * from manifestacao where codigo = " + str(codigo)
             manifestacoes = listarBancoDados(conexao, comandoSql)
 
-            if len(manifestacoes) == 0:
+            if codigo == 0:
+                print()
+                print('Voltando...')
+            elif len(manifestacoes) == 0:
                 print('Não existe manifestação com este código!')
             else:
                 for i in manifestacoes:
@@ -18,6 +20,6 @@ def pesquisarManifestacaoPorCodigo(conexao):
                     print('Descrição:', i[2])
                     print('Tipo:', i[3])
                     print('Autor:', i[4])
-
+                codigo = 0
         except ValueError:
-            print('Não existe manifestação com este código!')
+            print('Código inválido!')
