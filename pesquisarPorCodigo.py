c8cdf96a921d4c9cb3f203eeb5f40d2d99fa27ef
Marcleidson <marcleidsoncontato@gmail.com>
2023-06-25 23:09:29 -0300

    Código reduzido 100% Funcional, removi as duplicidades de comando desnecessárias.

--- pesquisarPorCodigo.py
+++ pesquisarPorCodigo.py
@@ -1,29 +1,17 @@
 from operacoesbd import *
 
 def pesquisarManifestacaoPorCodigo(conexao):
-    comandoSql = "select count(*) from manifestacao"
-    manifestacoes = listarBancoDados(conexao, comandoSql)
-    manifestacoes = manifestacoes[0][0]
 
-    if manifestacoes == 0:
-        print('Não há manifestações a serem listadas!')
-
-    else:
-        codigo = input('Digite o código da manifestação que desejada listar: ')
+        codigo = int(input('Digite o código da manifestação que desejada listar: '))
         print()
 
         try:
-            int(codigo)
-            comandoSql = "select count(*) from manifestacao where codigo = " + codigo
+            comandoSql = "select * from manifestacao where codigo = " + str(codigo)
             manifestacoes = listarBancoDados(conexao, comandoSql)
-            manifestacoes = manifestacoes[0][0]
 
-            if manifestacoes == 0:
+            if len(manifestacoes) == 0:
                 print('Não existe manifestação com este código!')
             else:
-                comandoSql = "select * from manifestacao where codigo = " + codigo
-                manifestacoes = listarBancoDados(conexao, comandoSql)
-
                 for i in manifestacoes:
                     print('Código:', i[0])
                     print('Título:', i[1])
